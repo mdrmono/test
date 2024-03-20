d4579e902373c397b708c0cbce1a479b962f35c8
RenatoRiosRomero <163938250+RenatoRiosRomero@users.noreply.github.com>
2024-03-20 15:48:01 -0500

    renato123
    renato123

--- get_requests.py
+++ get_requests.py
@@ -5,5 +5,5 @@ import pandas as pd
 
 response = requests.get("https://covid-api.com/api/reports").json()
 df2 = pd.DataFrame(response["data"])
-df2.to_csv("data_renato.csv")
+df2.to_csv("data_renato123.csv")
 print(df2)
